f5a2240d0c3266e88d4f3c1e888d9a3de0baa73f
David Lam <dav_lam@ymail.com>
2018-04-09 15:02:08 -0400

    added stubbed webserver

---
+++ src/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const SimpleNodeLogger = require('simple-node-logger');
+
+opts = {
+    logFilePath: "./logs/server.log",
+    timestampFormat: "YYYY-MM-DD HH:mm:ss"
+};
+const logger = SimpleNodeLogger.createSimpleLogger(opts);
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+
+app.use(express.static(path.join(__dirname, "/build"), {maxAge: "1w"}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+/**
+* Health endpoint.
+*
+*/
+app.get("/health", (request, response) => {
+  logger.info(`hostname: ${request.hostname}`);
+  logger.info(`ip: ${request.ip}`);
+
+  response.status(200)
+    .send("<h1> Server is Up </h1>");
+});
+
+app.put("/register", (request, response) => {
+  logger.info("REGISTERING User")
+});
+
+/*
+  Listen to connections.
+*/
+app.listen(port, (error) => {
+  if (error) {
+    logger.error(error);
+  } else {
+    logger.info(`Webserver is LIVE. ${port}`);
+  }
+});
