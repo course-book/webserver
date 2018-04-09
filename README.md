d58bea9d76def043e22c4ba77e4bb891c223a90e
David Lam <dav_lam@ymail.com>
2018-04-09 15:24:06 -0400

    address potential race condition with logging setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Course-Book's Core Backend Server
+## Development
+Modify `src/server.js` and add your desired endpoint.
+Run `yarn dev` or `npm run dev`.
--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const SimpleNodeLogger = require('simple-node-logger');
+const SimpleNodeLogger = require("simple-node-logger");
+const mkdirp = require("mkdirp")
 
-opts = {
-    logFilePath: "./logs/server.log",
-    timestampFormat: "YYYY-MM-DD HH:mm:ss"
-};
-const logger = SimpleNodeLogger.createSimpleLogger(opts);
+// Setup
+mkdirp("./logs", (error) => {
+  if (error) {
+    console.log("Unable to construct logs directory");
+    process.exit(1);
+  }
+  initialize();
+});
+
+// Use Basic Logger. Have it override if setup completes.
+let logger = SimpleNodeLogger.createSimpleLogger();
+
 const app = express();
-
 const port = process.env.PORT || 8080;
-
 
 app.use(express.static(path.join(__dirname, "/build"), {maxAge: "1w"}));
 
@@ -26,24 +32,30 @@ app.get("/health", (request, response) => {
   logger.info(`hostname: ${request.hostname}`);
   logger.info(`ip: ${request.ip}`);
 
-  response.status(200)
-    .send("<h1> Server is Up </h1>");
+  response.status(200).send("<h1> Server is Up </h1>");
 });
 
+/**
+* Handle User Registration.
+*/
 app.put("/register", (request, response) => {
   logger.info("REGISTERING User");
 
-  response.status(201)
-    .send("<h1> Register request received </h1>");
+  response.status(201).send("<h1> Register request received </h1>");
 });
 
-/*
-  Listen to connections.
-*/
-app.listen(port, (error) => {
-  if (error) {
-    logger.error(error);
-  } else {
-    logger.info(`Webserver is LIVE. ${port}`);
-  }
-});
+const initialize = () => {
+  const opts = {
+    logFilePath: "./logs/server.log",
+    timestampFormat: "YYYY-MM-DD HH:mm:ss"
+  };
+  logger = SimpleNodeLogger.createSimpleLogger(opts);
+
+  app.listen(port, (error) => {
+    if (error) {
+      logger.error(error);
+    } else {
+      logger.info(`Webserver is LIVE. ${port}`);
+    }
+  });
+}
